adc69b1d48c48ffa4a488a53a5d48e70eb42da8f
akki1071993 <akashmahant143@gmail.com>
2022-05-31 12:23:07 +0530

    commit from local 31/05/2022 12:23 PM

--- JavaPracticeProject/src/akash/searchEmail/searchEmaiId.java
+++ JavaPracticeProject/src/akash/searchEmail/searchEmaiId.java
@@ -1,29 +1,61 @@
 package akash.searchEmail;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class searchEmaiId {
 
 	public static void main(String[] args) {
-		String Array[] = { "qwe.gmail.com", "xrtyye.gmail.com", "uhh.gmail.com", "ghjk.gmail.com", "j.gmail.com",
-				"lkj.gmail.com", "okjj.gmail.com", "rgkm.gmail.com", "ojh.gmail.com", "rjp.gmail.com",
-				"plk.gmail.com" };
-		
-		String searchinString = "";
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter your email Id");
-		searchinString = s.nextLine();
-		
-		for (String i : Array) {
-			
-			if(i.equals(searchinString)) {
-				System.out.println("Email found");
-				break;
-			}
-			else {
-				System.out.println("Email not found");
-			}
-						
+
+		List<String> email = new ArrayList<String>();
+
+		email.add("akash@gmail.com");
+		email.add("vijay@gmail.com");
+		email.add("shubham@gmail.com");
+		email.add("Dinesh143@gmail.com");
+		email.add("pushpa62@gmail.com");
+		email.add("jay56@gmail.com");
+		email.add("mala@gmail.com");
+		email.add("kushpPatel@gmail.com");
+
+		try {
+			String emailPattern = "^(.+)@(.+)$";
+			Pattern pattern = Pattern.compile(emailPattern);
+
+			String searchinString;
+			int count = 0;
+			Scanner s = new Scanner(System.in);
+
+			System.out.println("Enter your email Id: ");
+			searchinString = s.nextLine();
+
+			Matcher matcher = pattern.matcher(searchinString);
+			if (matcher.matches()) {
+				for (String string : email) {
+					if (string.compareTo(searchinString) == 0) {
+						count = 0;
+						break;
+					} else
+						count++;
+				}
+
+				if (count == 0)
+					System.out.println("\"" + searchinString + "\"" + " is successfully found ! ");
+				else
+					System.out.println("Sorry \"" + searchinString + "\"" + " is not found ! ");
+				
+				
+
+			} else
+				System.out.println("\"" + searchinString + "\"" + " is not valid email.\nPlease try again !");
+		} catch (Exception e) {
+			System.out.println("Sorry somthing is worng");
+			System.out.println("Error:" + e.getMessage());
+		} finally {
+			System.out.println("\n\nHappy to serve you ! See you again !");
 		}
 
 	}
